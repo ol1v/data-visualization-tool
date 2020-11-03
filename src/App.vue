743198c8c73379d6dd83321bcccca5e4a6a7467a
Oliver Lennartsson <oliverhenriklennartsson@gmail.com>
2020-11-03 12:49:45 +0100

    first commit, playing with linechart

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <RandomChart />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import RandomChart from "./components/RandomChart";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    RandomChart,
+  },
+};
 </script>
 
 <style>
+body, html {
+  background-color: #74ebd5;
+  background-image: linear-gradient(180deg, #1f9696 0%, #101011 100%);
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  width: 100%;
+  height: 100%;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -24,5 +31,6 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  
 }
 </style>
---
+++ src/components/RandomChart.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="small">
+    <line-chart :chart-data="datacollection"></line-chart>
+    <button @click="fillData()">Randomize</button>
+    <button>Add dataset</button>
+  </div>
+</template>
+<script>
+import LineChart from "./LineChart.js";
+
+export default {
+  components: {
+    LineChart,
+  },
+  data() {
+    return {
+      datacollection: null,
+    };
+  },
+  mounted() {
+    this.fillData();
+  },
+  methods: {
+    fillData() {
+      this.datacollection = {
+        labels: [this.getRandomInt(), this.getRandomInt()],
+        datasets: [
+          {
+            label: "Data One",
+            backgroundColor: "#f87979",
+            data: [this.getRandomInt(), this.getRandomInt()],
+          },
+          {
+            label: "Data One",
+            backgroundColor:
+              "#1f9696",
+            data: [this.getRandomInt(), this.getRandomInt()],
+          },
+        ],
+      };
+    },
+    getRandomInt() {
+      return Math.floor(Math.random() * (50 - 5 + 1)) + 5;
+    },
+  },
+};
+</script>
+
+<style>
+.small {
+  max-width: 600px;
+  padding: 3em;
+  margin: 150px auto;
+  background-color: #031f315e;
+  border-radius: 0.5em;
+}
+</style>
